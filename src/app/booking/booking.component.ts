424b3232814f2ff56b22c51278c9ce98a2aeb2a7
radek <radekpietrzak102@gmail.com>
2020-12-27 15:18:52 +0100

    check free rooms redirection added

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-booking',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
+
+  redirectToRoomList(): void {
+    this.router.navigateByUrl('/room-list');
+  }
 
   ngOnInit(): void {
   }
