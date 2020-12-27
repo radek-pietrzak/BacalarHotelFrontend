965d2e517680cd13b0bbe87483387eb6b4903fcb
radek <radekpietrzak102@gmail.com>
2020-12-27 09:34:03 +0100

    create new component navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule
